027b62d378e4916fd5e4094aff341ec58af53e39
pacestefano <135709394+pacestefano@users.noreply.github.com>
2024-08-16 14:04:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 
 // Inserisci la tua chiave API qui in chiaro
-const apiKey = 'sk-Y2EQ243dTXiAiVk8gDOSI9QMnj6mttVu7ZkVCuwZTZT3BlbkFJBTqJ4N6S0nLo1jxibk_J0mihDl0HQ0elVyOb6FKssA';
+const apiKey = 'sk-NZsdjhu7jEQmD7EcGkNMjTNPCgX7A2Ku-Pdi0iby4AT3BlbkFJc0DkWjiDnABhx0F9BYGvusVeseT3IE7BNdDmy4THIA';
 
 // Codifica la chiave API in base64 per offuscarla
 const encodedApiKey = btoa(apiKey);
